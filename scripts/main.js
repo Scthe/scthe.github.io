e502fbf55a41d12772a34b04397533a800456537
Scthe <marcin1113C@gmail.com>
2015-08-12 19:00:59 +0200

    init

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // dynamically created references
+    var post_links = document.querySelectorAll(".post a"),
+        post_element = document.getElementsByClassName('post');
+    if (post_element.length > 0 && post_links.length > 0) {
+        post_element = post_element[0];
+        var header = document.createElement("h2"),
+            link_container = document.createElement("ul");
+        header.textContent = "Links";
+
+        for (var i = 0; i < post_links.length; i++) {
+            var l = post_links[i];
+            if(!l.href || l.href.indexOf(window.location.host + window.location.pathname) !== -1)
+                continue;
+
+            // console.log(l);
+            var link_el = document.createElement("li"),
+                anchor_el = document.createElement("a"),
+                title_el = document.createElement("div");
+            anchor_el.setAttribute('href', l.href);
+            anchor_el.textContent = l.href;
+            title_el.textContent = l.title.length > 0 ? l.title : l.text;
+            link_el.appendChild(anchor_el);
+            link_el.appendChild(title_el);
+            link_container.appendChild(link_el);
+        }
+        post_element.appendChild(header);
+        post_element.appendChild(link_container);
+    }
+
+    // anchors for post's headings
+    anchors.options = {
+        placement: 'right',
+        visible: 'always',
+        icon: '§',
+        class: 'content__anchor'
+    };
+    anchors.add('.post h2');
+});
